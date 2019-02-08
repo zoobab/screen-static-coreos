dabcf7638ddcd80069f0e05bae210dde1747a836
Heiner Peuser <heiner@MacBook.local>
2019-02-08 08:03:36 +0100

    Upgraded version to 4.6.2

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 WORKDIR="$PWD/bin"
 mkdir -pv $WORKDIR
 docker build -t screen-static-coreos:latest .
-docker run -v $WORKDIR:/mnt screen-static-coreos:latest cp -v /home/core/screen-4.4.0/screen /mnt
+docker run -v $WORKDIR:/mnt screen-static-coreos:latest cp -v /home/core/screen-4.6.2/screen /mnt
