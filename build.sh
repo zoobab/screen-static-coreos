db24485e37ed461ffabfbb91fb109060e4116384
MaxPeal <30347730+MaxPeal@users.noreply.github.com>
2020-08-03 12:32:45 +0200

    fix comments

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 WORKDIR="$PWD/bin"
 mkdir -pv $WORKDIR
-VER=$(grep "ENV VER" Dockerfile | cut -d" " -f3)
+VER=$(grep "ENV VER" Dockerfile | grep -v '^[[:space:]]*#' | cut -d" " -f3)
 docker build -t screen-static-coreos:latest .
 docker run -v $WORKDIR:/mnt screen-static-coreos:latest cp -v /home/core/screen-$VER/screen /mnt
 docker run -v $WORKDIR:/mnt screen-static-coreos:latest cp -v /home/core/screen-$VER/screen-upx /mnt
